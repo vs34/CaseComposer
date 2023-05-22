8288e2113bf9a9410c8ab861c09c4d7b998a8930
vansh22559 <132361456+vansh22559@users.noreply.github.com>
2023-05-22 18:03:15 +0530

    Update tcc.py
    No need to delete csv file again & -2

--- tcc.py
+++ tcc.py
@@ -1,21 +1,26 @@
 import os
 import subprocess
 n=0
+
+with open("tc1.csv", "w") as opfile:
+    pass
+
+
 for a in range(100):
     # Read the multi-line input from tc.py
     input_data = subprocess.run(["python3", "tc.py"], stdout=subprocess.PIPE, text=True).stdout
     print(a)
 # Run the input data through working.out and store the output
-    working_output = subprocess.check_output(["./working.out"], input=input_data, text=True).replace(" ", "").replace("\n", "")
+    working_output = subprocess.check_output(["./working.out"], input=input_data, text=True).replace("\n", " ")
 
 # Run the input data through notworking.out and store the output
-    not_working_output = subprocess.check_output(["./notworkin.out"], input=input_data, text=True).replace(" ", "").replace("\n", "")
+    not_working_output = subprocess.check_output(["./notworkin.out"], input=input_data, text=True).replace("\n", " ")
     # print(not_working_output,working_output)
     # print(not_working_output==working_output)
 # Write the output to separate files
     with open("tc1.csv", "a") as opfile:
         if working_output != not_working_output:
-            opfile.write("{},{},{}\n".format(input_data.strip(), not_working_output.strip(), working_output.strip()))
+            opfile.write("{} ,{} ,{} \n".format(input_data.strip(), not_working_output.strip(), working_output.strip()))
             opfile.write("++++++++++++++++++++++++++++++++++++++++++++++\n")
             n += 1
 
