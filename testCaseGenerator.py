225594847551a6c752e207f1634f9a6fe39a149e
shamiksinha <shamiksinha2003@outlook.com>
2025-02-14 21:30:46 +0530

    udpated

--- testCaseGenerator.py
+++ testCaseGenerator.py
@@ -1,27 +1,29 @@
-import random
+# SAMPLE TEST CASE GENERATOR
 
-def generate_test_cases(t: int):
-    print(t)
-    total_n = 0
-    MAX_N = 2 * 10 # Maximum allowed sum of n
-    MAX_SINGLE_N = 10  # Maximum n in a single test case
+# import random
 
-    for _ in range(t):
-        remaining_n = MAX_N - total_n  # Calculate remaining `n` that can be used
-        if remaining_n <= 0:  # Stop generating if the total limit is reached
-            break
+# def generate_test_cases(t: int):
+#     print(t)
+#     total_n = 0
+#     MAX_N = 2 * 10 # Maximum allowed sum of n
+#     MAX_SINGLE_N = 10  # Maximum n in a single test case
 
-        upper_bound = min(remaining_n, MAX_SINGLE_N)  # Ensure upper bound is valid
-        n = random.randint(1, upper_bound) if upper_bound >= 1 else 1  # Avoid invalid range
-        m = 1  # Always 1 as per problem constraints
-        total_n += n
+#     for _ in range(t):
+#         remaining_n = MAX_N - total_n  # Calculate remaining `n` that can be used
+#         if remaining_n <= 0:  # Stop generating if the total limit is reached
+#             break
+
+#         upper_bound = min(remaining_n, MAX_SINGLE_N)  # Ensure upper bound is valid
+#         n = random.randint(1, upper_bound) if upper_bound >= 1 else 1  # Avoid invalid range
+#         m = 1  # Always 1 as per problem constraints
+#         total_n += n
         
-        a = [random.randint(1, 10) for _ in range(n)]
-        b = [random.randint(1, 10) for _ in range(m)]
+#         a = [random.randint(1, 10) for _ in range(n)]
+#         b = [random.randint(1, 10) for _ in range(m)]
         
-        print(n, m)
-        print(" ".join(map(str, a)))
-        print(" ".join(map(str, b)))
+#         print(n, m)
+#         print(" ".join(map(str, a)))
+#         print(" ".join(map(str, b)))
 
-# Example usage: Generate up to 100 test cases
-generate_test_cases(1)
+# # Example usage: Generate up to 100 test cases
+# generate_test_cases(1)
--- wrong.cpp
+++ wrong.cpp
@@ -1,58 +1,51 @@
-#pragma GCC optimize("O3,unroll-loops")
-#include<bits/stdc++.h>
-using namespace std;
+//SAMPLE CODE TO TEST
 
-#define int long long
-#define no  cout << "No" << endl;
-#define yes cout << "Yes" << endl;
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a)*(b)/gcd(a, b))
-#define lp(a,n)  for(int i =a; i< n; i++)
-#define print(x) for(auto i: x) { cout << i << " "; }; cout << "\n";
+// #pragma GCC optimize("O3,unroll-loops")
+// #include<bits/stdc++.h>
+// using namespace std;
 
-#define MOD 1000000007;
+// #define int long long
+// #define lp(a,n)  for(int i =a; i< n; i++)
 
-void solve(){
-    int n, m; cin >> n >> m;
-    vector<int> a(n), b(m);
+// void solve(){
+//     int n, m; cin >> n >> m;
+//     vector<int> a(n), b(m);
 
-    lp(0, n){
-        cin >> a[i];
-    }
-    lp(0, m){
-        cin >> b[i];
-    }
+//     lp(0, n){
+//         cin >> a[i];
+//     }
+//     lp(0, m){
+//         cin >> b[i];
+//     }
 
-    // can have two values a[i] and b[0] - a[i] ;
-    // must always be a[i-1] <= a[i]
+//     // can have two values a[i] and b[0] - a[i] ;
+//     // must always be a[i-1] <= a[i]
 
-    a[0] = min(a[0], b[0] - a[0]);
+//     a[0] = min(a[0], b[0] - a[0]);
 
-    lp(1, n){
-        int first = a[i];
-        int second = b[0] - a[i];
+//     lp(1, n){
+//         int first = a[i];
+//         int second = b[0] - a[i];
 
-        if(a[i-1] <= first){
-            if(a[i-1] <= second){
-                a[i] = min(first, second);
-            }
-        }else if(a[i-1] <= second){
-            a[i] = second;
-        }else{
-            no;
-            return;
-        }
-    }
+//         if(a[i-1] <= first){
+//             if(a[i-1] <= second){
+//                 a[i] = min(first, second);
+//             }
+//         }else if(a[i-1] <= second){
+//             a[i] = second;
+//         }else{
+//             no;
+//             return;
+//         }
+//     }
 
-    yes;
-}
+//     yes;
+// }
 
-signed main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    int x = 1;
-    cin >> x;
-    while(x--){
-        solve();
-    }
-}
+// signed main() {
+//     int x = 1;
+//     cin >> x;
+//     while(x--){
+//         solve();
+//     }
+// }
